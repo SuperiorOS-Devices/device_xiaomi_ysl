7b5a3819846ca502d0533d0f9d7fbe8edd291f64
ItsMeVedansh <jaivadensh@gmail.com>
2021-05-30 00:11:47 +0000

    Fixed Conflict And sysfs errors

--- prop.mk
+++ prop.mk
@@ -242,7 +242,7 @@ persist.vendor.ims.disableDebugDataPathLogs=1 \
 persist.vendor.ims.disableDebugLogs=1 \
 persist.vendor.ims.disableSigHandler=1 \
 persist.vendor.ims.disableQXDMLogs=1
-=======
+
 # Ram Management
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.config.fha_enable=true \
